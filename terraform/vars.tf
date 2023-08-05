7dcfa9ca088af31ce4668ff27b0b3529721819d1
goksha <shashigokhale@gmail.com>
2023-08-05 17:32:08 +0100

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,1 +1,23 @@
+variable "cluster_name" {
+  type      = string
+  description = "cluster name"
+  default   = "k8s-cluster"
+}
 
+variable "region" {
+  type = string
+  description = "cluster region"
+  default = "us-central1"
+}
+
+variable "location" {
+  type        = string
+  description = "cluster location"
+  default     = "us-central1-a"
+}
+
+variable "project_id" {
+  type = string
+  description = "project ID"
+  default = "eco-tenure-393916"
+}
