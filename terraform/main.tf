67efc3452610eaa9665ec3734d2b3fc54fd76764
goksha <shashigokhale@gmail.com>
2023-08-14 11:24:07 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,40 +46,5 @@ provider "kubectl" {
   token                  = module.gke_auth.token
   load_config_file       = false
 }
-/*
-data "kubectl_file_documents" "namespace" {
-    content = file("../manifests/argocd/namespace.yaml")
-} 
 
-data "kubectl_file_documents" "argocd" {
-    content = file("../manifests/argocd/install.yaml")
-}
-
-resource "kubectl_manifest" "namespace" {
-    count     = length(data.kubectl_file_documents.namespace.documents)
-    yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
-    override_namespace = "argocd"
-}
-
-resource "kubectl_manifest" "argocd" {
-    depends_on = [
-      kubectl_manifest.namespace,
-    ]
-    count     = length(data.kubectl_file_documents.argocd.documents)
-    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
-    override_namespace = "argocd"
-}
-
-data "kubectl_file_documents" "my-nginx-app" {
-    content = file("../manifests/argocd/my-nginx-app.yaml")
-}
-
-resource "kubectl_manifest" "my-nginx-app" {
-    depends_on = [
-      kubectl_manifest.argocd,
-    ]
-    count     = length(data.kubectl_file_documents.my-nginx-app.documents)
-    yaml_body = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
-    override_namespace = "argocd"
-} */
 
